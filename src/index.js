8ba0590fa46548930b24a8ceadc847ab88580fa2
Acla Putra <muhamadacla22@gmail.com>
2021-09-07 06:13:25 +0700

    fix index

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Footer from './Footer'
 import './styles/index.css';
 import App from './App';
 
@@ -8,7 +7,6 @@ import App from './App';
 ReactDOM.render(
   <React.StrictMode>
     <App />
-    <Footer />
   </React.StrictMode>,
   document.getElementById('root')
 );
