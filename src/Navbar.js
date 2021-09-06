bd97f827f7bc41703327c0c507c02c90ecbe23ed
Acla Putra <muhamadacla22@gmail.com>
2021-09-07 05:23:00 +0700

    added navbar

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import './styles/Navbar.css'
+import './styles/responsive.css'
 
 function Navbar() {
     return (
@@ -10,9 +11,9 @@ function Navbar() {
                     <Link to="/" className="logo no-deco">ACLAPUTRA</Link>
                 </div>
                 <div className="right__content">
-                    <Link to="/about" className="about no-deco">ABOUT</Link>
-                    <Link to="/projects" className="no-deco">PROJECTS</Link>
-                    <Link to="/blog" className="no-deco">BLOG</Link>                                         
+                    <Link to="/about" className="about no-deco link_right_content">ABOUT</Link>
+                    <Link to="/projects" className="no-deco link_right_content">PROJECTS</Link>
+                    <Link to="/blog" className="no-deco link_right_content">BLOG</Link>                                         
                 </div>
             </div>
         </div>
