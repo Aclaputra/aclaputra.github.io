79ba11a1678409790dcc9535c72e31d75a65cba9
Acla Putra <muhamadacla22@gmail.com>
2021-09-08 01:01:45 +0700

    modified Projects

--- src/routes/Projects.js
+++ src/routes/Projects.js
@@ -1,9 +1,30 @@
 import React from 'react'
-
+import '../styles/Projects.css'
+import '../styles/responsive.css'
+import Project from './Project'
 function Projects() {
     return (
-        <div>
-            Projects
+        <div className="projects">
+            <div className="upper__projects">
+                <h1>PROJECTS</h1>
+            </div>
+            <div className="list__projects">
+                <div className="list__project">
+                    <Project className="project__content"/>
+                </div>
+                <div className="list__project">
+                    <Project className="project__content"/>
+                </div>
+                <div className="list__project">
+                    <Project className="project__content"/>
+                </div>
+                <div className="list__project">
+                    <Project className="project__content"/>
+                </div>
+                <div className="list__project">
+                    <Project className="project__content"/>
+                </div>
+            </div>
         </div>
     )
 }
