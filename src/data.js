037c66f04204bcb1813e901cb15f84141b9fe514
Aclaputra <muhamadacla22@gmail.com>
2022-09-07 07:31:10 +0700

    feat: added data dummy

---
+++ src/data.js
@@ -0,0 +1,18 @@
+const getData = () => {
+  return [
+    {
+      id: 1,
+      name: "Chatbot App",
+      date: "3 May 2022 - 27 May 2022",
+      stacks: "Flask, Docker, Kubernetes, Cloud Run, Terraform, GCP, Linux"
+    },
+    {
+      id: 2,
+      name: "Employment Management",
+      date: "29 Jun 2022 - 2 July 2022",
+      stacks: "Laravel, React, Linux"
+    }
+  ];
+}
+
+export { getData };
