0e8fd17e3d35289d625ee06d263e5bfb11eacc21
Aclaputra <muhamadacla22@gmail.com>
2022-09-07 07:31:49 +0700

    feat: added src/routes/ProjectItemBody.js

---
+++ src/routes/ProjectItemBody.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import CardContent from '@material-ui/core/CardContent';
+import Typography from '@material-ui/core/Typography';
+
+export default function ProjectItemBody({ name, date, stacks }) {
+  return (
+    <>
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="h2">{name}</Typography>
+        <Typography variant="body2" color="textSecondary" component="h5">{date}</Typography>
+        <Typography variant="body2" color="textSecondary" component="h5">{stacks}</Typography>
+      </CardContent>
+    </>
+  );
+}
