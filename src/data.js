0c4c39f8305e4e59df024f6b883967fd2de47dc0
Aclaputra <muhamadacla22@gmail.com>
2022-09-07 08:53:33 +0700

    feat: modified dummy data

--- src/data.js
+++ src/data.js
@@ -2,15 +2,30 @@ const getData = () => {
   return [
     {
       id: 1,
-      name: "Chatbot App",
+      name: "Chatbot App (Capstone Traveloka)",
       date: "3 May 2022 - 27 May 2022",
-      stacks: "Flask, Docker, Kubernetes, Cloud Run, Terraform, GCP, Linux"
+      imageUri: "./sc_chatbot.jpeg",
+      stacks: "Flask, Docker, Kubernetes, Cloud Run, Firebase, Terraform, GCP, Jupyter Notebook, Kotlin",
+      linkWeb: "https://github.com/Bangkit-Capstone-C22-CB03-Team/TVLK-Chatbot",
+      linkGithub: "https://github.com/Bangkit-Capstone-C22-CB03-Team/Bangkit-C22-CB03-Backend"
     },
     {
       id: 2,
-      name: "Employment Management",
+      name: "Employment Management (Simple Fullstack App)",
       date: "29 Jun 2022 - 2 July 2022",
-      stacks: "Laravel, React, Linux"
+      imageUri: "./sc_employmentManagement.png",
+      stacks: "Laravel, React, MySQL",
+      linkWeb: "",
+      linkGithub: "https://github.com/Aclaputra/employment-management"
+    },
+    {
+      id: 3,
+      name: "Portfolio (Single Page Application)",
+      date: "29 Jun 2022 - 2 July 2022",
+      imageUri: "./sc_portfolio.png",
+      stacks: "React, Material UI",
+      linkWeb: "https://inspiring-agnesi-1524be.netlify.app/",
+      linkGithub: "https://github.com/Aclaputra/acla.my.id"
     }
   ];
 }
