fbd4613db79135e7d1f16a71790ab7a85accc880
Aclaputra <muhamadacla22@gmail.com>
2022-09-07 07:31:19 +0700

    feat: added projectapp

---
+++ src/routes/ProjectApp.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ProjectList from './ProjectList.js';
+import { getData } from '../data';
+import '../styles/Projects.css';
+import '../styles/responsive.css';
+
+export default function ProjectApp() {
+  const projects = getData();
+
+  return (
+    <div className="projects">
+      <div className="upper__projects">
+        <h1>PROJECTS</h1>
+      </div>
+      <div className="list__projects">
+        <ProjectList projects={projects} />
+      </div>
+    </div>
+  );
+}
