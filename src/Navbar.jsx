8f122ee8c9e2507640236b16850170c6bd55121b
Aclaputra <muhamadacla22@gmail.com>
2022-09-07 07:30:05 +0700

    feat: added navbar

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -4,7 +4,7 @@ import './styles/responsive.css'
 import About from './routes/About'
 import {BrowserRouter as Router, Switch, Link, Route } from 'react-router-dom'
 import Footer from './Footer'
-import Projects from './routes/Projects'
+import ProjectApp from './routes/ProjectApp'
 import Blog from './routes/Blog'
 
 function Navbar() {
@@ -25,7 +25,7 @@ function Navbar() {
             </div>
             <Switch>
                 <Route path="/" exact component={About}></Route>
-                <Route path="/projects" exact component={Projects}></Route>
+                <Route path="/projects" exact component={ProjectApp}></Route>
                 <Route path="/blog" exact component={Blog}></Route>
             </Switch>
 
