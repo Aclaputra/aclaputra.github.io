fe9356fcee0006b2201a81027ac6ab050a11de66
Acla Putra <muhamadacla22@gmail.com>
2024-01-03 19:59:57 +0700

    feat: add blog

--- src/routes/Blog.tsx
+++ src/routes/Blog.tsx
@@ -1,7 +1,23 @@
+import { Link } from 'react-router-dom'
+import '../styles/responsive.css'
+
 function Blog() {
     return (
-        <div>
-            Blog
+        <div className="blog">
+            <div className="header">
+                <h1>Blog</h1>
+                <p>This is a blog</p>
+            </div>
+            <div className="header">
+                <div className="header">
+                    <h2>1. How algorithms works ?</h2>
+                    <p>Description of how algorithms works</p>
+                </div>
+                <div className="header">
+                    <h2>2. Deep dive into Brute Force algorithms</h2>
+                    <p>Description of deep dive into brute force algorithms</p>
+                </div>
+            </div>
         </div>
     )
 }
